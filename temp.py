8cea28a959c05512ae5062debdbcd1c75da624f1
brewerj3 <100174522+brewerj3@users.noreply.github.com>
2023-11-30 12:21:34 -1000

    fixed formatting

--- temp.py
+++ temp.py
@@ -8,12 +8,12 @@ r_w = 0.57
 a = 3.9083E-3
 b = -5.775E-7
 
-thermocouple = MCP3008(channel = 0)
+thermocouple = MCP3008(channel=0)
 
 while True:
     sleep(1)
     r_t = (thermocouple.value * r_k) / (1 - thermocouple.value)
     r_f = r_t - r_w
-    t_c = (-a + sqrt(a ** 2 - 4 * b * (1 - r_f/r_0))) / (2 * b)
-    t_f = 1.8 * t_c + 32
-    print(f'Resistance (Ohms): {r_f:.3f} Temperature (Fahrenheit): {t_f:.3f}')
+    t_c = (-a + sqrt(a ** 2 - 4 * b * (1 - r_f / r_0))) / (2 * b)
+    t_f = 9/5 * t_c + 32
+    print(f'Resistance (Ohms): {r_f:.3f} Temperature (Fahrenheit): {t_f:.3f} Tempertature (Celsius): {t_c:.3f}')
